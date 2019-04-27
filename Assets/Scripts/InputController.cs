52ee51aa1c48e2f82a97f416f4a091049d688c21
Michael Shemesh <michaelshemesh@Michaels-MBP.poly.edu>
2019-04-27 17:29:45 -0400

    added buttons

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UniRx;
+using System;
+
+public class InputController : MonoBehaviour
+{
+    EventHandler BoseWearableEventHandler; 
+
+    // Use this for initialization
+    void Start()
+    {
+        Bose.Wearable.WearableControl.Instance.HeadNodGesture.Enable();
+        Bose.Wearable.WearableControl.Instance.HeadShakeGesture.Enable();
+        //BoseWearableEventHandler += Bose.Wearable.WearableControl.Instance.DoubleTapDetected;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Left()
+    {
+     
+    }
+
+    public void Right()
+    {
+
+    }
+
+    public void Yes()
+    {
+
+    }
+
+    public void No()
+    {
+
+    }
+}
