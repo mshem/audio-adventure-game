af74e9ae04cac820f238632c75899026defa51cb
ringo kam <ringo_kam@hotmail.com>
2019-04-27 13:10:47 -0400

    ringo stuff

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public enum MyState 
+{
+    DoNothing,
+    TriggerAlienDialog,
+    TriggerPlayerAwaitAction  
+}
+
+public class CurrentEvent
+{
+    public Queue<SoundQueue> SoundQueue;
+    public int? Goto;
+    public KeyValuePair<bool, int> Decision;
+}
+
+public class SoundQueue
+{
+    public bool IsRepeat;
+    public string FileLoc;
+    public bool IsBlocking;
+    public string TransformObject;
+}
+
+public class GameManager : MonoBehaviour
+{
+    Subject<MyState> myState = new Subject<MyState>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //jsonutil load file
+        //var file = loadFile
+        //var 
+
+        myState.Subscribe(state =>
+        {
+            switch (state)
+            {
+                case MyState.DoNothing:
+                    //Do nothing
+                    break;
+                case MyState.TriggerAlienDialog:
+                    //fetch current state
+                    //trigger soudQueueCoroutinen w/ current state (within here it will add to sound queue or trigger playerawaitactionstate
+                    //set MyState.DoNothing 
+                    break;
+                case MyState.TriggerPlayerAwaitAction:
+                    break;
+            }
+        });
+        myState.Publish(MyState.TriggerAlienDialog);
+    }
+}
